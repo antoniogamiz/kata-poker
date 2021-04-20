d8998e6f210f5982605968fe7dbf4cff427c07a8
Antonio Gamiz Delgado <antoniogamiz>
2021-04-20 21:26:22 +0200

    feat: detect royal flush

--- src/use_cases/hand_detector.py
+++ src/use_cases/hand_detector.py
@@ -1,5 +1,20 @@
 from src.entities.deck import Deck, Card
+from src.entities.hand import Hand
 
 
-def detect_royal_flush(cards: [Card]):
-    pass
+class HandDetector:
+    def detect_hand(self, hand: [Card]):
+        hand_type = self._detect_royal_flush(hand)
+        return hand_type or Hand.Empty
+
+    def _detect_royal_flush(self, hand: [Card]):
+        ordered_hand = hand.sort(key=lambda x: x.number)
+        cards_ids = [card.number for card in hand]
+        if cards_ids[0] == 0 and self._check_integer_ascending_sequence(cards_ids[1:]) or self._check_integer_ascending_sequence(cards_ids):
+            return Hand.RoyalFlush
+
+        return None
+
+    def _check_integer_ascending_sequence(self, array):
+        normalized_array = [array[i] - array[0] - i for i in range(len(array))]
+        return len(set(normalized_array)) == 1
--- tests/use_cases/test_detect_hand_use_cases.py
+++ tests/use_cases/test_detect_hand_use_cases.py
@@ -1,13 +1,41 @@
 import unittest
+from src.entities.deck import Card
 from src.entities.hand import Hand
-from src.use_cases.hand_detector import detect_royal_flush
+from src.use_cases.hand_detector import HandDetector
 
 
 class TestDetectHandUseCase(unittest.TestCase):
 
     def test_royal_flush(self):
-        hand = []
-        self.assertEqual(detect_royal_flush(hand), Hand.RoyalFlush)
+        hand_detector = HandDetector()
+        hand = self._create_royal_flush()
+        self.assertEqual(hand_detector.detect_hand(hand), Hand.RoyalFlush)
 
-    def _create_royal_flush():
-        return [Card(2, 1), Card(3, 1), Card(4, 1), Card(5, 1), Card(6, 1)]
+    def test_best_royal_flush(self):
+        hand_detector = HandDetector()
+        hand = self._create_best_royal_flush()
+        self.assertEqual(hand_detector.detect_hand(hand), Hand.RoyalFlush)
+
+    def test_unordered_royal_flush(self):
+        hand_detector = HandDetector()
+        hand = self._create_unordered_royal_flush()
+        self.assertEqual(hand_detector.detect_hand(hand), Hand.RoyalFlush)
+
+    def test_empty_hand(self):
+        hand_detector = HandDetector()
+        hand = self._create_empty_hand()
+        self.assertEqual(hand_detector.detect_hand(hand), Hand.Empty)
+
+    def _create_royal_flush(self):
+        return [Card(1, i, 0) for i in range(5)]
+
+    def _create_best_royal_flush(self):
+        hand = [Card(1, i+9, 0) for i in range(4)]
+        hand.append(Card(1, 0, 0))
+        return hand
+
+    def _create_unordered_royal_flush(self):
+        return [Card(1, 5, 0), Card(1, 4, 0), Card(1, 2, 0), Card(1, 3, 0), Card(1, 1, 0)]
+
+    def _create_empty_hand(self):
+        return [Card(1, 7, 0), Card(2, 4, 0), Card(3, 2, 0), Card(3, 3, 0), Card(0, 1, 0)]
