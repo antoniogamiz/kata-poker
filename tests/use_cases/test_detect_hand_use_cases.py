e62a12557a8362f14441d574c34fe6307b73c944
Antonio Gamiz Delgado <antoniogamiz>
2021-04-21 15:57:25 +0200

    refactor: creation of common hands

--- tests/use_cases/test_detect_hand_use_cases.py
+++ tests/use_cases/test_detect_hand_use_cases.py
@@ -3,113 +3,69 @@ import unittest
 from src.entities.deck import Card
 from src.entities.hand import Hand
 from src.use_cases.hand_detector import HandDetector
+from tests.utils.hand_factory import HandFactory
 
 
 class TestDetectHandUseCase(unittest.TestCase):
     hand_detector = HandDetector()
+    hand_factory = HandFactory()
 
     def test_royal_flush(self):
-        hand = self._create_royal_flush()
+        hand = self.hand_factory.create_royal_flush()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.RoyalFlush)
 
     def test_almost_royal_flush(self):
-        hand = self._create_almost_royal_flush()
+        hand = self.hand_factory.create_almost_royal_flush()
         expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.Straight)
+        self.assertEqual(expected_hand, Hand.HighCard)
 
     def test_straight_flush(self):
-        hand = self._create_straight_flush()
+        hand = self.hand_factory.create_straight_flush()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.StraightFlush)
 
     def test_unordered_royal_flush(self):
-        hand = self._create_unordered_royal_flush()
+        hand = self.hand_factory.create_unordered_royal_flush()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.RoyalFlush)
 
     def test_four_of_a_kind(self):
-        hand = self._create_four_of_a_kind()
+        hand = self.hand_factory.create_four_of_a_kind()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.FourOfAKind)
 
     def test_full_house(self):
-        hand = self._create_full_house()
+        hand = self.hand_factory.create_full_house()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.FullHouse)
 
     def test_flush(self):
-        hand = self._create_flush()
+        hand = self.hand_factory.create_flush()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.Flush)
 
     def test_straight(self):
-        hand = self._create_straight()
+        hand = self.hand_factory.create_straight()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.Straight)
 
     def test_three_of_a_kind(self):
-        hand = self._create_three_of_a_kind()
+        hand = self.hand_factory.create_three_of_a_kind()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.ThreeOfAKind)
 
     def test_two_pair(self):
-        hand = self._create_two_pair()
+        hand = self.hand_factory.create_two_pair()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.TwoPair)
 
     def test_one_pair(self):
-        hand = self._create_one_pair()
+        hand = self.hand_factory.create_one_pair()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.OnePair)
 
     def test_high_card(self):
-        hand = self._create_high_card()
+        hand = self.hand_factory.create_high_card()
         expected_hand = self.hand_detector.detect_hand(hand)
         self.assertEqual(expected_hand, Hand.HighCard)
-
-    def _create_royal_flush(self):
-        return [Card(1, i, 0) for i in range(5)]
-
-    def _create_almost_royal_flush(self):
-        return [Card(1+i, i, 0) for i in range(5)]
-
-    def _create_straight_flush(self):
-        hand = [Card(1, i+8, 0) for i in range(5)]
-        return hand
-
-    def _create_unordered_royal_flush(self):
-        return [Card(1, 0, 0), Card(1, 4, 0), Card(1, 2, 0), Card(1, 3, 0), Card(1, 1, 0)]
-
-    def _create_four_of_a_kind(self):
-        hand = [Card(i, 1, 0) for i in range(4)]
-        hand.append(Card(1, 0, 0))
-        return hand
-
-    def _create_full_house(self):
-        hand = [Card(i, 10, 0) for i in range(3)]
-        hand.append(Card(1, 9, 0))
-        hand.append(Card(2, 9, 0))
-        return hand
-
-    def _create_flush(self):
-        return [Card(1, 1, 0), Card(1, 3, 0), Card(
-            1, 4, 0), Card(1, 5, 0), Card(1, 6, 0)]
-
-    def _create_straight(self):
-        return [Card(1+i, i, 0) for i in range(5)]
-
-    def _create_three_of_a_kind(self):
-        return [Card(1, 1, 0), Card(2, 1, 0), Card(
-            3, 1, 0), Card(2, 2, 0), Card(1, 3, 0)]
-
-    def _create_two_pair(self):
-        return [Card(1, 1, 0), Card(2, 1, 0), Card(
-            1, 2, 0), Card(2, 2, 0), Card(1, 3, 0)]
-
-    def _create_one_pair(self):
-        return [Card(1, 6, 0), Card(2, 1, 0), Card(
-            1, 2, 0), Card(2, 2, 0), Card(1, 3, 0)]
-
-    def _create_high_card(self):
-        return [Card(1, 7, 0), Card(2, 4, 0), Card(3, 2, 0), Card(3, 3, 0), Card(0, 1, 0)]
---
+++ tests/utils/hand_factory.py
@@ -0,0 +1,87 @@
+from src.entities.deck import Card
+
+
+class HandFactory:
+    def create_royal_flush(self):
+        return [Card(1, 0, 12),
+                Card(1, 9, 8),
+                Card(1, 10, 9),
+                Card(1, 11, 10),
+                Card(1, 12, 11)]
+
+    def create_almost_royal_flush(self):
+        return [Card(1, 0, 12),
+                Card(1, 9, 8),
+                Card(1, 10, 9),
+                Card(1, 11, 10),
+                Card(2, 12, 11)]
+
+    def create_straight_flush(self):
+        return [Card(1, 8, 7),
+                Card(1, 9, 8),
+                Card(1, 10, 9),
+                Card(1, 11, 10),
+                Card(1, 12, 11)]
+
+    def create_unordered_royal_flush(self):
+        return [Card(1, 0, 12),
+                Card(1, 9, 8),
+                Card(1, 11, 10),
+                Card(1, 10, 9),
+                Card(1, 12, 11)]
+
+    def create_four_of_a_kind(self):
+        return [Card(0, 1, 12),
+                Card(1, 1, 0),
+                Card(2, 1, 0),
+                Card(3, 1, 0),
+                Card(1, 2, 1)]
+
+    def create_full_house(self):
+        return [Card(0, 10, 9),
+                Card(1, 10, 9),
+                Card(2, 10, 9),
+                Card(1, 11, 10),
+                Card(2, 11, 10)]
+
+    def create_flush(self):
+        return [Card(0, 5, 4),
+                Card(0, 2, 1),
+                Card(0, 4, 3),
+                Card(0, 11, 10),
+                Card(0, 1, 0)]
+
+    def create_straight(self):
+        return [Card(0, 1, 0),
+                Card(1, 2, 1),
+                Card(2, 3, 2),
+                Card(3, 4, 3),
+                Card(0, 5, 4)]
+
+    def create_three_of_a_kind(self):
+        return [Card(0, 0, 12),
+                Card(1, 0, 12),
+                Card(2, 0, 12),
+                Card(3, 11, 10),
+                Card(0, 1, 0)]
+
+    def create_two_pair(self):
+        return [Card(0, 0, 12),
+                Card(1, 0, 12),
+                Card(2, 3, 2),
+                Card(3, 3, 2),
+                Card(0, 1, 0)]
+
+    def create_one_pair(self):
+        return [Card(0, 0, 12),
+                Card(1, 0, 12),
+                Card(2, 3, 2),
+                Card(3, 11, 10),
+                Card(0, 1, 0)]
+
+    def create_high_card(self):
+        return [Card(0, 0, 12),
+                Card(1, 5, 4),
+                Card(2, 3, 2),
+                Card(3, 11, 10),
+                Card(0, 1, 0)]
