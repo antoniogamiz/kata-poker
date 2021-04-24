2ed816c6ad5b5537388f1798bb4fbf344c29a015
Antonio Gamiz Delgado <antoniogamiz>
2021-04-24 09:57:33 +0200

    refactor(test): improve HandParser test suite

--- tests/use_cases/test_detect_hand_use_cases.py
+++ tests/use_cases/test_detect_hand_use_cases.py
@@ -1,71 +1,93 @@
 import unittest
 
-from src.entities.deck import Card
+from src.entities.deck import Suit, Pip, Card
 from src.entities.hand import Hand
-from src.use_cases.hand_detector import HandDetector
+from src.use_cases.hand_parser import HandParser
 from tests.utils.hand_factory import HandFactory
 
 
-class TestDetectHandUseCase(unittest.TestCase):
-    hand_detector = HandDetector()
-    hand_factory = HandFactory()
+class TestHandParserUseCase(unittest.TestCase):
 
-    def test_royal_flush(self):
-        hand = self.hand_factory.create_royal_flush()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.RoyalFlush)
+    def test_when_the_hand_is_a_royal_flush(self):
+        hand = [Card(Suit.SPADES, Pip.ACE),
+                Card(Suit.SPADES, Pip.TEN),
+                Card(Suit.SPADES, Pip.J)
+                Card(Suit.SPADES, Pip.K)
+                Card(Suit.SPADES, Pip.Q)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.RoyalFlush)
 
-    def test_almost_royal_flush(self):
-        hand = self.hand_factory.create_almost_royal_flush()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.HighCard)
+    def test_when_the_hand_is_a_straight_flush(self):
+        hand = [Card(Suit.SPADES, Pip.NINE),
+                Card(Suit.SPADES, Pip.TEN),
+                Card(Suit.SPADES, Pip.J)
+                Card(Suit.SPADES, Pip.K)
+                Card(Suit.SPADES, Pip.Q)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.StraightFlush)
 
-    def test_straight_flush(self):
-        hand = self.hand_factory.create_straight_flush()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.StraightFlush)
+    def test_when_the_hand_is_a_four_of_a_kind(self):
+        hand = [Card(Suit.DIAMONDS, Pip.ACE),
+                Card(Suit.CLUBS, Pip.ACE),
+                Card(Suit.HEARTS, Pip.ACE)
+                Card(Suit.SPADES, Pip.ACE)
+                Card(Suit.SPADES, Pip.EIGHT)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.FourOfAKind)
 
-    def test_unordered_royal_flush(self):
-        hand = self.hand_factory.create_unordered_royal_flush()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.RoyalFlush)
+    def test_when_the_hand_is_a_full_house(self):
+        hand = [Card(Suit.DIAMONDS, Pip.ACE),
+                Card(Suit.CLUBS, Pip.ACE),
+                Card(Suit.HEARTS, Pip.ACE)
+                Card(Suit.SPADES, Pip.TEN)
+                Card(Suit.CLUBS, Pip.TEN)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.FullHouse)
 
-    def test_four_of_a_kind(self):
-        hand = self.hand_factory.create_four_of_a_kind()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.FourOfAKind)
+    def test_when_the_hand_is_a_flush(self):
+        hand = [Card(Suit.SPADES, Pip.NINE),
+                Card(Suit.SPADES, Pip.TWO),
+                Card(Suit.SPADES, Pip.SIX)
+                Card(Suit.SPADES, Pip.FOUR)
+                Card(Suit.SPADES, Pip.Q)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.Flush)
 
-    def test_full_house(self):
-        hand = self.hand_factory.create_full_house()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.FullHouse)
+    def test_when_the_hand_is_a_straight(self):
+        hand = [Card(Suit.SPADES, Pip.NINE),
+                Card(Suit.SPADES, Pip.TEN),
+                Card(Suit.CLUBS, Pip.J)
+                Card(Suit.SPADES, Pip.K)
+                Card(Suit.DIAMONDS, Pip.Q)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.Straight)
 
-    def test_flush(self):
-        hand = self.hand_factory.create_flush()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.Flush)
+    def test_when_the_hand_is_a_three_of_a_kind(self):
+        hand = [Card(Suit.SPADES, Pip.NINE),
+                Card(Suit.SPADES, Pip.TEN),
+                Card(Suit.CLUBS, Pip.NINE)
+                Card(Suit.SPADES, Pip.K)
+                Card(Suit.DIAMONDS, Pip.NINE)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.ThreeOfAKind)
 
-    def test_straight(self):
-        hand = self.hand_factory.create_straight()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.Straight)
+    def test_when_the_hand_is_a_two_pair(self):
+        hand = [Card(Suit.SPADES, Pip.NINE),
+                Card(Suit.CLUBS, Pip.TEN),
+                Card(Suit.CLUBS, Pip.NINE)
+                Card(Suit.SPADES, Pip.TEN)
+                Card(Suit.DIAMONDS, Pip.Q)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.TwoPair)
 
-    def test_three_of_a_kind(self):
-        hand = self.hand_factory.create_three_of_a_kind()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.ThreeOfAKind)
+    def test_when_the_hand_is_a_one_pair(self):
+        hand = [Card(Suit.SPADES, Pip.NINE),
+                Card(Suit.CLUBS, Pip.TEN),
+                Card(Suit.CLUBS, Pip.EIGHT)
+                Card(Suit.SPADES, Pip.TEN)
+                Card(Suit.DIAMONDS, Pip.Q)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.OnePair)
 
-    def test_two_pair(self):
-        hand = self.hand_factory.create_two_pair()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.TwoPair)
+    def test_when_the_hand_is_a_high_card(self):
+        hand = [Card(Suit.SPADES, Pip.FIVE),
+                Card(Suit.CLUBS, Pip.TEN),
+                Card(Suit.CLUBS, Pip.EIGHT)
+                Card(Suit.SPADES, Pip.TEN)
+                Card(Suit.DIAMONDS, Pip.Q)]
+        self._assert_hand_is_correctly_parsed(hand, Hand.HighCard)
 
-    def test_one_pair(self):
-        hand = self.hand_factory.create_one_pair()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.OnePair)
-
-    def test_high_card(self):
-        hand = self.hand_factory.create_high_card()
-        expected_hand = self.hand_detector.detect_hand(hand)
-        self.assertEqual(expected_hand, Hand.HighCard)
+    def _assert_hand_is_correctly_parsed(self, hand, expected_hand):
+        detected_hand = HandParser.parse(hand)
+        self.assertEqual(parsed_hand, expected_hand)
