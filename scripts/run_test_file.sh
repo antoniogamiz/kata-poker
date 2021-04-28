9bd6e0c2b8852d86e0a1f1a9ed6b8818b083b3ee
antoniogamiz <antoniogamiz10@gmail.com>
2021-04-28 20:41:49 +0200

    config: allow running tests from src and test files

--- scripts/run_test_file.sh
+++ scripts/run_test_file.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-TEST_MODULE_PATH=$(echo "$1" | tr / . | sed -r 's/(src\.|\.py)//g' | rev | sed -e 's/\./_tset\./' | rev)
 TEST_DIRECTORY="tests"
 
-PYTHONPATH=${workspaceRoot}/src python3 -m unittest "${TEST_DIRECTORY}.${TEST_MODULE_PATH}"
+FILE=$1
+
+if [[ $1 == src/tests* ]] ;
+then
+    SRC_MODULE_PATH=$(echo "$FILE" | tr / . | sed -r 's/(src\.|\.py)//g')
+    MODULE_TO_RUN="${SRC_MODULE_PATH}"
+else
+    echo "Running test file"
+    TEST_MODULE_PATH=$(echo "$FILE" | tr / . | sed -r 's/(src\.|\.py)//g' | rev | sed -e 's/\./_tset\./' | rev)
+    MODULE_TO_RUN="${TEST_DIRECTORY}.${TEST_MODULE_PATH}"
+fi
+
+PYTHONPATH=${workspaceRoot}/src python3 -m unittest $MODULE_TO_RUN
